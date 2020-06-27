09b534148affe023167bf3e2c71f9ea00eb39f0a
Ragahd217 <norahbarjas2000@gmail.com>
2020-06-27 21:08:37 +0300

    finish

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,23 +1,42 @@
 // السؤال الأول
-// 1
+// 1 
+const name = "رغد"
 
 // 2
-
+let age = 17
 // 3 and 4
+console.log( age+"و عمري" +name + "انا" ) 
+
 
 // 5 and 6
+console.log( age * 5 )
 
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // السؤال الثاني
 // 1
+Array = ['draw','plant',age]
 
 // 2
+Array.push("17")
+
+console.log(Array )
+
 
 // 3
+Array.pop(4)
+console.log(Array)
 
 // 4
+const me = {
+    track:"web",
+    lang:"css" }
 
+    console.log(me.lang)
+
+
+      
+      
 // 5
 
 // 6
